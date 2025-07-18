99311f6f292cc31ff7f3b21bd9a56e9deffabdc6
mutti <marco.mutti@elmec.it>
2025-07-18 11:23:11 +0200

    fix: debug options

--- django_pylibmc/memcached.py
+++ django_pylibmc/memcached.py
@@ -84,9 +84,9 @@ class PyLibMCCache(BaseMemcachedCache):
         self.binary = int(params.get("BINARY", False))
         
         self._local = local()
-        self._options = params.get("OPTIONS", None)
-        if self._options is not None:
-            self._options = self._options.get("behaviors", None)
+        # self._options = params.get("OPTIONS", None)
+        # if self._options is not None:
+        #     self._options = self._options.get("behaviors", None)
             
         super(PyLibMCCache, self).__init__(
             self._server,
@@ -111,6 +111,7 @@ class PyLibMCCache(BaseMemcachedCache):
             )
         client = self._lib.Client(self._servers, **client_kwargs)
         if self._options:
+            log.info(f"Loading pylibmc options: {self._options}")
             client.behaviors = self._options
 
         self._local.client = client

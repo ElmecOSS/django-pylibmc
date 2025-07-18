47d12bbdbacff0eb709eccbc45cbbec16a3cf50e
mutti <marco.mutti@elmec.it>
2025-07-18 17:24:23 +0200

    fix: add new check on OPTIONS' keys

--- django_pylibmc/memcached.py
+++ django_pylibmc/memcached.py
@@ -131,6 +131,13 @@ class PyLibMCCache(BaseMemcachedCache):
         ):
 
             self._behaviors = self._new_params.get("OPTIONS", {}).get("behaviors", None)
+            for key in self._new_params.get("OPTIONS", {}):
+                if key not in SUPPORTED_LYBMC_OPTIONS:
+                    log.warning(
+                        f"Option {key} is not supported, check https://sendapatch.se/projects/pylibmc/behaviors.html to see a list of supported options"
+                    )
+                    self._new_params.get("OPTIONS", {}).pop(key)
+
             for key, item in self._behaviors.items():
                 if key not in SUPPORTED_LYBMC_OPTIONS:
                     log.warning(
